c55443f35a31cf079a8f505c36ba0434f9031b0e
HCarlos <r0@tecnointel.mx>
2023-07-15 17:42:48 +0000

    SIAC - B21 | L7.30.6 Production

--- resources/views/SIAC/denuncia/denuncia/__denuncia/__denuncia_edit.blade.php
+++ resources/views/SIAC/denuncia/denuncia/__denuncia/__denuncia_edit.blade.php
@@ -307,12 +307,12 @@
     <div class="col-sm-6">
         <h6>ÚLTIMA RESPUESTA</h6>
         <address>
-            <strong class="orange">POR:</strong>  {{ $items->dependencias->sortDesc()->last()->dependencia }}<br>
-            <strong class="info">SERVICIO:</strong>  {{ $items->denuncia_servicios->sort()->last()->servicio }}<br>
-            <strong class="seagreen">ESTATUS:</strong>  {{ $items->denuncia_estatus->sortDesc()->first()->estatus }}<br>
-            <strong class="red">FAVORABLE:</strong>  {{ $items->denuncia_estatus->sortDesc()->first()->pivot->favorable == true ? 'SI' : 'NO' }}<br>
-            <strong class="purple">FECHA:</strong>  {{ date('d-m-Y H:i:s', strtotime($items->denuncia_estatus->sortDesc()->first()->pivot->fecha_movimiento)) }}<br>
-            <strong class="coral">RESPUESTA:</strong> <small> {{ $items->denuncia_estatus->sortDesc()->first()->pivot->observaciones }} </small><br>
+            <strong class="orange">POR:</strong>  {{ $items->ultimo_estatu_denuncia_dependencia_servicio->sort()->last()->dependencia->dependencia }}<br>
+            <strong class="info">SERVICIO:</strong>  {{ $items->ultimo_estatu_denuncia_dependencia_servicio->sort()->last()->servicio->servicio }}<br>
+            <strong class="seagreen">ESTATUS:</strong>  {{ $items->ultimo_estatu_denuncia_dependencia_servicio->sort()->last()->estatu->estatus }}<br>
+            <strong class="red">FAVORABLE:</strong>  {{ $items->ultimo_estatu_denuncia_dependencia_servicio->sort()->last()->favorable == true ? 'SI' : 'NO' }}<br>
+            <strong class="purple">FECHA:</strong>  {{ date('d-m-Y H:i:s', strtotime($items->ultimo_estatu_denuncia_dependencia_servicio->sort()->last()->fecha_movimiento)) }}<br>
+            <strong class="coral">RESPUESTA:</strong> <small> {{ $items->ultimo_estatu_denuncia_dependencia_servicio->sort()->last()->observaciones }} </small><br>
         </address>
     </div> <!-- end col-->
 </div>
