25bce39ba0c1d0cb7c3a6e81bd93942d70d0fd0e
HCarlos <r0@tecnointel.mx>
2023-08-23 13:38:35 +0000

    SIAC - B34 | L7.30.6 Production

--- resources/views/partials/script_footer.blade.php
+++ resources/views/partials/script_footer.blade.php
@@ -42,11 +42,11 @@
 
 {{--<script src="http://localhost:{{env('LARAVEL_ECHO_PORT')}}/socket.io/socket.io.js"  ></script>--}}
 
-{{--<script src="{{env('HOST_REDIS')}}:{{env('LARAVEL_ECHO_PORT')}}/socket.io/socket.io.js"  ></script>--}}
+<script src="{{env('HOST_REDIS')}}:{{env('LARAVEL_ECHO_PORT')}}/socket.io/socket.io.js"  ></script>
 
 {{--<script src="//{{ Request::getHost() }}:{{env('LARAVEL_ECHO_PORT')}}/socket.io/socket.io.js"></script>--}}
 
-<script src="http://{{ Request::getHost() }}:6001/socket.io/socket.io.js"></script>
+{{--<script src="http://{{ Request::getHost() }}:6001/socket.io/socket.io.js"></script>--}}
 
 
 {{--<script src="{{ url('/js/socket.io.js') }}" type="text/javascript" ></script>--}}
