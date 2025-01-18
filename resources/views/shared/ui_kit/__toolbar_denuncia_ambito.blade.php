f368f925e99123655e5b666fd30fd168ad16f726
HCarlos <r0@tecnointel.mx>
2025-01-18 01:53:56 +0000

    SIAC - C42w_f | L7.30.7 Production

--- resources/views/shared/ui_kit/__toolbar_denuncia_ambito.blade.php
+++ resources/views/shared/ui_kit/__toolbar_denuncia_ambito.blade.php
@@ -1,26 +1,21 @@
 <div class="button-list pt-1 pb-1">
     @isset($newItem)
-{{--        @php dd(\Illuminate\Support\Facades\Auth::user()->isPermission('csd_sas|consultar|all')) @endphp--}}
-        @if( \Illuminate\Support\Facades\Auth::user()->isPermission('csd_sas|consultar|all') )
+        @if( \Illuminate\Support\Facades\Auth::user()->isPermission('crear_expediente_servicios_municipales|crear_expediente_apoyos_sociales|all') )
             <a href="{{route($newItem)}}" class="btn btn-outline-white btn-rounded btn-sm ml-1" data-toggle="tooltip" data-placement="top" data-original-title="Nueva Solicitud">
                 <i class="fas fa-plus"></i> Nueva Solicitud
             </a>
         @endif
     @endisset
     @isset($showProcess1)
-{{--        <a href="{{ route($showProcess1)}}" class="btn btn-icon btn-outline-success ml-1 btn-rounded btnGetItems" data-toggle="tooltip" data-placement="top" data-original-title="Exportar a MS Excel">--}}
-{{--            <i class="fas fa-file-excel text-white"></i>--}}
-{{--        </a>--}}
-            <button type="button" class="btn btn-icon btn-outline-white ml-1 btn-rounded dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <i class="fas fa-file-excel"></i>
-            </button>
-            <div class="dropdown-menu">
-                @php $indice = -1 @endphp
-                @foreach(config("atemun.menu_archivos") as $key=>$value )
-                    <a class="dropdown-item btnGetItems" href="{{ route($showProcess1)}}-{{$value}}-{{++$indice}}">{{$key}}</a>
-                @endforeach
-            </div>
-
+        <button type="button" class="btn btn-icon btn-outline-white ml-1 btn-rounded dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <i class="fas fa-file-excel"></i>
+        </button>
+        <div class="dropdown-menu">
+            @php $indice = -1 @endphp
+            @foreach(config("atemun.menu_archivos") as $key=>$value )
+                <a class="dropdown-item btnGetItems" href="{{ route($showProcess1)}}-{{$value}}-{{++$indice}}">{{$key}}</a>
+            @endforeach
+        </div>
     @endisset
     @isset($showModalSearchDenuncia)
         <span data-toggle="modal" data-target="#modalFull" >
@@ -29,10 +24,8 @@
             </a>
         </span>
     @endisset
-
-        <a href="" class="btn btn-icon btn-outline-white ml-1 btn-rounded" data-toggle="tooltip" data-placement="top" data-original-title="Actualizar" onclick="window.location.reload(true);">
-            <i class="fas fa-sync-alt"></i>
-        </a>
-
+    <a href="" class="btn btn-icon btn-outline-white ml-1 btn-rounded" data-toggle="tooltip" data-placement="top" data-original-title="Actualizar" onclick="window.location.reload(true);">
+        <i class="fas fa-sync-alt"></i>
+    </a>
 </div>
 
