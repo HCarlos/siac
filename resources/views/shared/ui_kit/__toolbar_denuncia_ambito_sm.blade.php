a65cba8c16ff6544c638c68aa13ebc995c905925
HCarlos <r0@tecnointel.mx>
2025-07-02 18:15:52 +0000

    SIAC - C44c3 | L7.30.7 Production

--- resources/views/shared/ui_kit/__toolbar_denuncia_ambito_sm.blade.php
+++ resources/views/shared/ui_kit/__toolbar_denuncia_ambito_sm.blade.php
@@ -1,7 +1,7 @@
 {{--<div class="button-list pt-2 pb-1 tbSM">--}}
 <div class="pt-2 pb-1 tbSM">
     @isset($newItem)
-        @if( \Illuminate\Support\Facades\Auth::user()->isPermission('crear_expediente_servicios_municipales|crear_expediente_apoyos_sociales|all') )
+        @if( Auth::user()->isPermission('crear_expediente_servicios_municipales|crear_expediente_apoyos_sociales|all') )
             <a href="{{url($newItem)}}" class="btn btn-outline-white btn-rounded btn-sm ml-1" data-toggle="tooltip" data-placement="top" data-original-title="Nueva Solicitud">
                 <i class="fas fa-plus"></i> Nueva Solicitud
             </a>
@@ -16,8 +16,10 @@
             @foreach(config("atemun.menu_archivos_sm") as $key=>$value )
                 <a class="dropdown-item btnGetItems" href="{{ route($showProcess1)}}-{{$value}}-{{++$indice}}">{{$key}}</a>
             @endforeach
-            <a class="dropdown-item disabled text-muted "></a>
-            <a class="dropdown-item " href="{{route('listDenunciasLocales')}}" >Solicitudes locales</a>
+             @if( Auth::user()->isPermission('Ver solicitudes locales|all') )
+                <a class="dropdown-item disabled text-muted "></a>
+                <a class="dropdown-item " href="{{route('listDenunciasLocales')}}" >Solicitudes locales</a>
+            @endcan
         </div>
     @endisset
     @isset($showModalSearchDenuncia)
